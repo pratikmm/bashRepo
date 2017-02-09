76b8ad0502c36866da5a2fa2dbabddd502658d95
pratikmm <pratikmm@hotmail.com>
2017-02-10 01:07:59 +0530

    Update sshMultiHost.sh

--- sshMultiHost.sh
+++ sshMultiHost.sh
@@ -1,5 +1,7 @@
 # bashRepo
-sed -e "s/,/\n/g" < hosts > host_file
-echo "Enter Command"
+#hosts is file in which hostnames are stored in comma separated format e.g user1@hostname,user2@hostname.
+sed -e "s/,/\n/g" < hosts > host_file #replaces commas ',' which new line charactes '\n' and store it in host_file file
+echo "Enter Command" #takes commands to be run on connected machines e.g pwd ; ls ; whoami
 read command
-pssh -i -h host_file $command
+pssh -i -h host_file $command #pssh command which will take host names from host_file and connect all the hosts and executes the command
+#passworldless ssh must be configured for these users.
