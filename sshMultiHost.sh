5a5ac15ab33108a2e8e4590b22d39be5c5e17dd0
pratikmm <pratikmm@hotmail.com>
2017-02-11 01:04:43 +0530

    Update sshMultiHost.sh

--- sshMultiHost.sh
+++ sshMultiHost.sh
@@ -1,4 +1,6 @@
-# bashRepo
+# here we are using pssh command to ssh into multiple hotst parallely and execute the given commands.
+#install pssh using following command
+#yum -y install pssh
 #hosts is file in which hostnames are stored in comma separated format e.g user1@hostname,user2@hostname.
 sed -e "s/,/\n/g" < hosts > host_file #replaces commas ',' which new line charactes '\n' and store it in host_file file
 echo "Enter Command" #takes commands to be run on connected machines e.g pwd ; ls ; whoami
